99fd1e6b83dd39c3c5c2db0ea30d2d5856f2cff1
chegree <chegree@CHBC324>
2022-03-09 08:56:26 +1300

    Add project files.

---
+++ ChemicalTestingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChemicalTestingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Menu-Instructions-Help
+            Console.WriteLine("    --------------Welcome to Chemical Testing--------------\n" +
+                "This App is used to test efficenty of Chemicals when killing bugs\n\n" +
+                "Help: 1.You can pick weather you would like to enter a random amount of germs\n" +
+                " or if you would like to pick your own amount of germs You need to enter the \n" +
+                "chemical you would like to test.");
+        }
+    }
+}
